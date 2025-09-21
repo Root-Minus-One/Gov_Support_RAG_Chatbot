b06ae4fc4d11d3e1e90985912e58487988b72a31
Ruthvik <ruthminusone@gmail.com>
2025-09-21 19:47:33 +0530

    Update chat_utils.py

--- app/chat_utils.py
+++ app/chat_utils.py
@@ -3,8 +3,6 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
 # !pip install -q langchain-google-genai
 
-# Configure API key
-genai.configure(api_key="AIzaSyCjOMCMBOOS7e6s0n2zAvWQZDgbxqFYBJ8")
 
 # Create the model
 model = genai.GenerativeModel("gemini-2.0-flash")
